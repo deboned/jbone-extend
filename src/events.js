1b56ecb2c54eb6f388699af2c95699b516ba665e
Paul <devel.pa@gmail.com>
2015-04-14 22:38:46 +0200

    Fixed events binding.

--- src/events.js
+++ src/events.js
@@ -2,10 +2,10 @@
  * Created by Paul on 4/14/2015.
  */
 
-jBone.each( ("blur focus focusin focusout load resize scroll unload click dblclick " +
-  "mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
-  "change select submit keydown keypress keyup error contextmenu").split(" "),
-  function( i, name ) {
+("blur focus focusin focusout load resize scroll unload click dblclick " +
+"mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
+"change select submit keydown keypress keyup error contextmenu").split(" ").forEach(
+  function( name ) {
 
     // Handle event binding
     jBone.fn[ name ] = function( data, fn ) {
